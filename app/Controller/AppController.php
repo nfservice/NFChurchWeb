7f70bec5b16aefec166f4d2039b88c375d46c08b
Bruno Gerotto <desenvolvimento2@nfservice.com.br>
2014-01-17 14:00:41 -0200

    [USERS / NFCHURCH] - Subindo estrutura incial

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -31,4 +31,11 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+	public $components = array(
+        'Session',
+        'Auth' => array(
+            'loginRedirect' => array('controller' => 'pages', 'action' => 'home'),
+            'logoutRedirect' => array('controller' => 'users', 'action' => 'login')
+        )
+    );
 }
