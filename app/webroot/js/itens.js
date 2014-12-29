9bccad3f656fc65b753c966198ae6186a398ea19
dodopok <dodopokcamilo@gmail.com>
2014-12-29 11:20:35 -0200

    [Biblioteca / UPD] Alterações Consulta Item

--- app/webroot/js/itens.js
+++ app/webroot/js/itens.js
@@ -1,26 +1,34 @@
 $(document).ready(function(){
 	$('#ItemAssunto').keypress(function(e){		
 		if (e.keyCode == 13) {
-			Pace.restart();
-			$.getJSON('searchItem/'+this.value, function(data){
-				$('#itensTable tr.item').remove();
-
-				$(data).each(function(i){
-					var item = data[i];
-					$('#itensTable').append(
-						'<tr class="item">'+
-							'<td>'+item.Item.titulo+' - '+item.Autor.nome+' ('+item.Tipo.nome+')</td>'+
-							'<td>'+item.Item.estoque+'</td>'+
-							'<td>'+
-								'<button onclick="modalLoad(\'emprestimo/'+item.Item.id+'\');" class="btn btn-primary">Empréstimo</button> '+
-								'<button onclick="modalLoad(\'devolucao/'+item.Item.id+'\');" class="btn btn-success">Devolução</button> '+
-								'<button onclick="modalLoad(\'historico/'+item.Item.id+'\');" class="btn btn-warning">Histórico</button>'+
-							'</td>'+
-						'</tr>'
-					);
-				});
-			});
 			e.preventDefault();
+			buscaItem();
 		}
 	});
 });
+
+function buscaItem(value) {
+	Pace.restart();
+	$.getJSON('searchItem/'+$('#ItemAssunto').val(), function(data){
+		$('#itensTable tr.item').remove();
+
+		$(data).each(function(i){
+			var item = data[i];
+			var line = '<tr class="item">'+
+				'<td>'+item.Item.titulo+' - '+item.Autor.nome+' ('+item.Tipo.nome+')</td>'+
+				'<td>'+item.Item.estoque+'</td>'+
+				'<td>';
+
+			if (item.Item.estoque > 0) {
+				line += '<button onclick="modalLoad(\'emprestimo/'+item.Item.id+'\');" class="btn btn-primary">Empréstimo</button> ';
+			}
+
+			line += '<button onclick="modalLoad(\'devolucao/'+item.Item.id+'\');" class="btn btn-success">Devolução</button> '+
+					'<button onclick="modalLoad(\'historico/'+item.Item.id+'\');" class="btn btn-warning">Histórico</button>'+
+				'</td>'+
+			'</tr>';
+
+			$('#itensTable').append(line);
+		});
+	});	
+}
